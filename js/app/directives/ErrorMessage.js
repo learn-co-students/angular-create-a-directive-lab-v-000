5b2ac21f26a227fb3eea8378b6cacad0bca64306
Katrina McNeely <katrina@Katrinas-MacBook-Pro.local>
2016-07-24 14:21:26 -0400

    Done.

---
+++ js/app/directives/ErrorMessage.js
@@ -0,0 +1,10 @@
+function ErrorMessage(){
+
+  return {
+    template: '<div><p>Oh no - something went wrong!</p></div>'
+  }
+}
+
+angular
+  .module('app')
+  .directive('errorMessage', ErrorMessage)
