9709d20a752e652ae5e3d16d9f7eafc3720839fc
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-02-13 13:11:27 -0500

    Done.

---
+++ js/app/directives/ErrorMessage.js
@@ -0,0 +1,14 @@
+
+function ErrorMessage() {
+	return {
+		template: [
+			'<div>',
+				'Oh no - something went wrong!',
+			'</div>'
+		].join('')
+	}
+}
+
+angular
+  .module('app')
+  .directive('errorMessage', ErrorMessage)
